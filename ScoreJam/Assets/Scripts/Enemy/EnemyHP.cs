dc0a618e7d41368d50963c5eccb843ae6b38fd18
Nikita <druzhbinn@gmail.com>
2023-01-01 02:04:03 +0300

    Small fixes

--- ScoreJam/Assets/Scripts/Enemy/EnemyHP.cs
+++ ScoreJam/Assets/Scripts/Enemy/EnemyHP.cs
@@ -1,34 +1,32 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyHP : MonoBehaviour
 {
-    [SerializeField] private int maxHP;
-    [SerializeField] private Rigidbody rb;
-    private int currentHP;
+    [SerializeField] private int maxHitPoints;
+    private Rigidbody _rb;
+    private int _currentHp;
     
     private GameObject lastCollided;
     private Vector3 lastVelocity, direction, summary;
     private float speed;
 
-    private void Awake() {
-        currentHP = maxHP;
-        rb = GetComponent<Rigidbody>();
+    private void Awake()
+    {
+        _currentHp = maxHitPoints;
+        _rb = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
-        lastVelocity = rb.velocity;
+        lastVelocity = _rb.velocity;
     }
 
     void OnCollisionEnter(Collision other)
     {
-        if(other.gameObject.tag == "Ammo")
+        if(other.gameObject.CompareTag("Ammo"))
         {
-            currentHP--;
-            if(currentHP == 0)
+            _currentHp--;
+            if(_currentHp == 0)
             {
                 Destroy(gameObject);
             }
@@ -39,7 +37,7 @@ public class EnemyHP : MonoBehaviour
                 speed = lastVelocity.magnitude;
             direction = Vector3.Reflect(lastVelocity.normalized, other.contacts[0].normal);
             summary = direction * Mathf.Max(speed, 0f) * 1.05f;
-            rb.velocity = new Vector3(Mathf.Min(summary.x, 20f), Mathf.Min(rb.velocity.y,20f), Mathf.Min(summary.z, 20f));
+            _rb.velocity = new Vector3(Mathf.Min(summary.x, 20f), Mathf.Min(_rb.velocity.y,20f), Mathf.Min(summary.z, 20f));
     }
 
     private void OnDestroy()
