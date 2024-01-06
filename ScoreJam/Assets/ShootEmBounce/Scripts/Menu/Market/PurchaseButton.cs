bdce55573c83e641dc9bb169d7a076675e81b268
Nikita <druzhbinn@gmail.com>
2024-01-06 18:06:09 +0300

    Save changes

--- ScoreJam/Assets/ShootEmBounce/Scripts/Menu/Market/PurchaseButton.cs
+++ ScoreJam/Assets/ShootEmBounce/Scripts/Menu/Market/PurchaseButton.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Localization;
+using TMPro;
 
 public class PurchaseButton : MonoBehaviour
 {
@@ -10,12 +11,11 @@ public class PurchaseButton : MonoBehaviour
     public LocalizedString selectedLocalizedString;
 
     private Button button;
-    private MarketLogic marketLogic;
+    [SerializeField] private MarketLogic marketLogic;
 
     private void Start()
     {
         button = GetComponent<Button>();
-        marketLogic = GetComponentInParent<MarketLogic>();
         UpdateButton();
     }
 
@@ -39,19 +39,36 @@ public class PurchaseButton : MonoBehaviour
             case ItemStatus.PurchasedSelected:
                 return selectedLocalizedString;
             default:
+                Debug.Log("default");
                 return new LocalizedString();
         }
     }
 
     private void SetButtonStyle(LocalizedString buttonText, bool interactable)
     {
-        button.interactable = interactable;
-        UpdateText(buttonText.GetLocalizedString());
+        if (button != null)
+        {
+            button.interactable = interactable;
+        }
+        else
+        {
+            Debug.LogError("Button is null. Make sure button is properly initialized.");
+        }
+
+        // Проверяем, не является ли buttonText null перед вызовом метода GetLocalizedString
+        if (buttonText != null)
+        {
+            UpdateText(buttonText.GetLocalizedString());
+        }
+        else
+        {
+            Debug.LogError("Button text is null. Make sure buttonText is properly initialized.");
+        }
     }
 
     private void UpdateText(string localizedString)
     {
-        button.GetComponentInChildren<Text>().text = localizedString;
+        button.GetComponentInChildren<TextMeshProUGUI>().text = localizedString;
     }
 
     public void OnButtonClick()
